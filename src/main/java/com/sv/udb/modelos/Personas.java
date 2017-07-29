e087ea12103ed121564ecd1c9826b5775314aa93
DanielWilfredo <DanielWilfredo@DanielWilfredo>
2017-07-29 10:44:00 -0600

    Terminado modelo falta controlador

---
+++ src/main/java/com/sv/udb/modelos/Personas.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.modelos;
+
+/**
+ *
+ * @author DanielWilfredo
+ */
+public class Personas {
+    private int codiPers;
+    private String nombPers;
+    private String apelPers;
+    private byte[] fotoPers;
+    private int codiTipoPers;
+    private char genePers;
+    private String fechaNaciPers;
+    private String duiPers;
+    private String nitPers;
+    private String tipoSangPers;
+    private int codiUbicPers;
+    private String fechAlta;
+    private String fechaBaja;
+    private int esta;
+
+    public int getCodiPers() {
+        return codiPers;
+    }
+
+    public void setCodiPers(int codiPers) {
+        this.codiPers = codiPers;
+    }
+
+    public String getNombPers() {
+        return nombPers;
+    }
+
+    public void setNombPers(String nombPers) {
+        this.nombPers = nombPers;
+    }
+
+    public String getApelPers() {
+        return apelPers;
+    }
+
+    public void setApelPers(String apelPers) {
+        this.apelPers = apelPers;
+    }
+
+    public byte[] getFotoPers() {
+        return fotoPers;
+    }
+
+    public void setFotoPers(byte[] fotoPers) {
+        this.fotoPers = fotoPers;
+    }
+
+    public int getCodiTipoPers() {
+        return codiTipoPers;
+    }
+
+    public void setCodiTipoPers(int codiTipoPers) {
+        this.codiTipoPers = codiTipoPers;
+    }
+
+    public char getGenePers() {
+        return genePers;
+    }
+
+    public void setGenePers(char genePers) {
+        this.genePers = genePers;
+    }
+
+    public String getFechaNaciPers() {
+        return fechaNaciPers;
+    }
+
+    public void setFechaNaciPers(String fechaNaciPers) {
+        this.fechaNaciPers = fechaNaciPers;
+    }
+
+    public String getDuiPers() {
+        return duiPers;
+    }
+
+    public void setDuiPers(String duiPers) {
+        this.duiPers = duiPers;
+    }
+
+    public String getNitPers() {
+        return nitPers;
+    }
+
+    public void setNitPers(String nitPers) {
+        this.nitPers = nitPers;
+    }
+
+    public String getTipoSangPers() {
+        return tipoSangPers;
+    }
+
+    public void setTipoSangPers(String tipoSangPers) {
+        this.tipoSangPers = tipoSangPers;
+    }
+
+    public int getCodiUbicPers() {
+        return codiUbicPers;
+    }
+
+    public void setCodiUbicPers(int codiUbicPers) {
+        this.codiUbicPers = codiUbicPers;
+    }
+
+    public String getFechAlta() {
+        return fechAlta;
+    }
+
+    public void setFechAlta(String fechAlta) {
+        this.fechAlta = fechAlta;
+    }
+
+    public String getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(String fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+
+    public int getEsta() {
+        return esta;
+    }
+
+    public void setEsta(int esta) {
+        this.esta = esta;
+    }
+
+    public Personas(int codiPers, String nombPers, String apelPers, byte[] fotoPers, int codiTipoPers, char genePers, String fechaNaciPers, String duiPers, String nitPers, String tipoSangPers, int codiUbicPers, String fechAlta, String fechaBaja, int esta) {
+        this.codiPers = codiPers;
+        this.nombPers = nombPers;
+        this.apelPers = apelPers;
+        this.fotoPers = fotoPers;
+        this.codiTipoPers = codiTipoPers;
+        this.genePers = genePers;
+        this.fechaNaciPers = fechaNaciPers;
+        this.duiPers = duiPers;
+        this.nitPers = nitPers;
+        this.tipoSangPers = tipoSangPers;
+        this.codiUbicPers = codiUbicPers;
+        this.fechAlta = fechAlta;
+        this.fechaBaja = fechaBaja;
+        this.esta = esta;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
